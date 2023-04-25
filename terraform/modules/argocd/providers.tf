dc6b70d671d8b852c3a421f0cdb96d2facd6c72d
ghazal-naderi <61361506+ghazal-naderi@users.noreply.github.com>
2023-04-25 09:07:15 +0100

    Create providers.tf

---
+++ terraform/modules/argocd/providers.tf
@@ -0,0 +1,31 @@
+provider "helm" {
+  kubernetes {
+    host                   = var.cluster_endpoint
+    cluster_ca_certificate = var.cluster_ca_certificate
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        var.cluster_name
+      ]
+    }
+  }
+}
+
+provider "kubernetes" {
+  host                   = var.cluster_endpoint
+  cluster_ca_certificate = var.cluster_ca_certificate
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      var.cluster_name
+    ]
+  }
+}
