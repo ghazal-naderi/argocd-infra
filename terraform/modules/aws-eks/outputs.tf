e2c49e5589247381b11e37632e4e3728804237e1
ghazal-naderi <61361506+ghazal-naderi@users.noreply.github.com>
2023-04-25 08:55:45 +0100

    Create outputs.tf

---
+++ terraform/modules/aws-eks/outputs.tf
@@ -0,0 +1,21 @@
+output "endpoint" {
+  value = aws_eks_cluster.cluster.endpoint
+}
+
+output "ca_cert" {
+  value = base64decode(aws_eks_cluster.cluster.certificate_authority.0.data)
+}
+
+output "iam_oidc_provider_endpoint" {
+  value = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+}
+
+output "cluster_iam_role_name" {
+  description = "IAM role name of the EKS cluster."
+  value       = aws_iam_role.eks_cluster.name
+}
+
+output "cluster_iam_role_arn" {
+  description = "IAM role ARN of the EKS cluster."
+  value       = aws_iam_role.eks_cluster.arn
+}
