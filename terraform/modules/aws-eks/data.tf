010ce9357d9a1f12096eab532c3ede1292f6dc8b
ghazal-naderi <61361506+ghazal-naderi@users.noreply.github.com>
2023-06-25 15:39:39 +0100

    Update data.tf

--- terraform/modules/aws-eks/data.tf
+++ terraform/modules/aws-eks/data.tf
@@ -1,14 +1,30 @@
-data "aws_vpc" "selected" {
-  id = var.vpc_id
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+  depends_on = [
+    aws_eks_cluster.cluster
+  ]
 }
 
-data "aws_subnets" "private" {
-  filter {
-    name   = "vpc-id"
-    values = [var.vpc_id]
-  }
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_roles" "admin" {
+  name_regex  = "AWSReservedSSO_AD-admin_.*"
+  path_prefix = "/aws-reserved/sso.amazonaws.com/${data.aws_region.current.name}"
 }
 
-data "aws_eks_cluster_auth" "eks" {
-  name = "${var.deploy_env}-${var.cluster_name}"
+data "aws_iam_roles" "dev" {
+  name_regex  = "AWSReservedSSO_AD-developer_.*"
+  path_prefix = "/aws-reserved/sso.amazonaws.com/${data.aws_region.current.name}"
+}
+
+data "aws_iam_roles" "user" {
+  name_regex  = "AWSReservedSSO_AWSPowerUserAccess_.*"
+  path_prefix = "/aws-reserved/sso.amazonaws.com/${data.aws_region.current.name}"
+}
+
+data "aws_iam_roles" "autoscaling" {
+  name_regex  = "AWSServiceRoleForAutoScaling.*"
+  path_prefix = "/aws-service-role/autoscaling.amazonaws.com"
 }
