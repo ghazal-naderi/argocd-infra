d1fa58d2159648d240a155bd5a5781bcafe0d636
ghazal-naderi <61361506+ghazal-naderi@users.noreply.github.com>
2023-06-25 15:39:06 +0100

    Update cluster.tf

--- terraform/modules/aws-eks/cluster.tf
+++ terraform/modules/aws-eks/cluster.tf
@@ -1,26 +1,39 @@
 resource "aws_eks_cluster" "cluster" {
-  name                      = "${var.deploy_env}-${var.cluster_name}"
-  role_arn                  = aws_iam_role.eks_cluster.arn
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster.arn
+  version  = var.kube_version
 
   vpc_config {
-    subnet_ids              = data.aws_subnets.private.ids
+    subnet_ids = var.private_subnet_ids
+    # endpoint_private_access = true
+    # endpoint_public_access  = false
   }
 
-  tags = merge(var.common_tags, var.additional_tags)
+  encryption_config {
+    provider {
+      key_arn = aws_kms_key.environment_kms_key.arn
+    }
+    resources = ["secrets"]
+  }
+
+  enabled_cluster_log_types = var.enabled_cluster_log_types
+
+  tags = merge(var.default_tags, { "os" = var.ami_type }, var.common_tags, var.additional_tags)
 
   depends_on = [
     aws_iam_role_policy_attachment.AmazonEKSVPCResourceController,
     aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
     aws_iam_role_policy_attachment.AmazonEKSServicePolicy,
     aws_iam_role_policy_attachment.ElasticLoadBalancingFullAccess,
+    aws_cloudwatch_log_group.control_plane_logging
   ]
 }
 
 # IAM
 resource "aws_iam_role" "eks_cluster" {
-  name               = "${var.deploy_env}-${var.cluster_name}-role"
+  name               = "${var.cluster_name}-eks-cluster-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy_for_eks_cluster.json
-  tags               = merge(var.common_tags, var.additional_tags)
+  tags               = merge(var.default_tags, var.common_tags, var.additional_tags)
 }
 
 data "aws_iam_policy_document" "assume_role_policy_for_eks_cluster" {
@@ -52,4 +65,12 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
 resource "aws_iam_role_policy_attachment" "ElasticLoadBalancingFullAccess" {
   policy_arn = "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"
   role       = aws_iam_role.eks_cluster.name
+}
+
+# Control plane logging
+resource "aws_cloudwatch_log_group" "control_plane_logging" {
+  name              = "/aws/eks/${var.cluster_name}/cluster"
+  retention_in_days = var.cluster_log_retention
+  kms_key_id        = aws_kms_key.environment_kms_key.arn
+  tags              = merge(var.default_tags, var.common_tags, var.additional_tags)
 }
