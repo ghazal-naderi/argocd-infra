3c37ca96eb1172a085b9fa8ba023a7cc75b30e67
ghazal-naderi <61361506+ghazal-naderi@users.noreply.github.com>
2023-06-25 15:41:57 +0100

    Create oidc.tf

---
+++ terraform/modules/aws-eks/oidc.tf
@@ -0,0 +1,10 @@
+data "tls_certificate" "eks" {
+  url = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+  tags            = merge(var.default_tags, var.common_tags, var.additional_tags)
+}
