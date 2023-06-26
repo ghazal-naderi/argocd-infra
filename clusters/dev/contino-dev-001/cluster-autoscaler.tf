c751f25f3fa8b34aa4f9a154615fe2e674701574
ghazal-naderi <61361506+ghazal-naderi@users.noreply.github.com>
2023-06-26 16:17:47 +0100

    Create cluster-autoscaler.tf

---
+++ clusters/dev/contino-dev-001/cluster-autoscaler.tf
@@ -0,0 +1,11 @@
+module "cluster-autoscaler" {
+  #source = "git::https://github.com/brevanhowardinfra/tf-aws-bh-eks-core-infra.git//terraform/modules/cluster-autoscaler?ref=tf-bh-argocd-0.2.0"
+  source = "../../../terraform/modules/cluster-autoscaler"
+
+  account_name                        = local.account_name
+  cluster_name                        = local.cluster_name
+  aws_iam_openid_connect_provider_url = module.eks_cluster.aws_iam_openid_connect_provider_url
+  aws_iam_openid_connect_provider_arn = module.eks_cluster.aws_iam_openid_connect_provider_arn
+
+  depends_on = [module.eks_cluster]
+}
