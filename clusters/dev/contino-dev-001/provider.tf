aa73ccc02dae62d113d750743bf9bae6f2e2c38f
ghazal-naderi <61361506+ghazal-naderi@users.noreply.github.com>
2023-06-26 16:17:01 +0100

    Create provider.tf

---
+++ clusters/dev/contino-dev-001/provider.tf
@@ -0,0 +1,59 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+provider "kubernetes" {
+  host                   = module.eks_cluster.endpoint
+  cluster_ca_certificate = module.eks_cluster.ca_cert
+  exec {
+    api_version = "client.authentication.k8s.io/v1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      local.cluster_name
+    ]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks_cluster.endpoint
+    cluster_ca_certificate = module.eks_cluster.ca_cert
+    exec {
+      api_version = "client.authentication.k8s.io/v1"
+      command     = "aws"
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        local.cluster_name
+      ]
+    }
+  }
+}
+
+provider "kubectl" {
+  host                   = module.eks_cluster.endpoint
+  cluster_ca_certificate = module.eks_cluster.ca_cert
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      local.cluster_name
+    ]
+  }
+}
+
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.10.0"
+    }
+  }
+}
