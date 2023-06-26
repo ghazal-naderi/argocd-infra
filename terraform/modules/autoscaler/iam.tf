5390e6570a401cebbd858041fdb84e46cb1110e8
ghazal-naderi <61361506+ghazal-naderi@users.noreply.github.com>
2023-06-26 16:09:29 +0100

    Create iam.tf

---
+++ terraform/modules/autoscaler/iam.tf
@@ -0,0 +1,68 @@
+data "aws_iam_policy_document" "cluster_autoscaler_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(var.aws_iam_openid_connect_provider_url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:cluster_autoscaler"]
+    }
+
+    principals {
+      identifiers = [var.aws_iam_openid_connect_provider_arn]
+      type        = "Federated"
+    }
+  }
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(var.aws_iam_openid_connect_provider_url, "https://", "")}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    principals {
+      identifiers = [var.aws_iam_openid_connect_provider_arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "cluster_autoscaler" {
+  assume_role_policy = data.aws_iam_policy_document.cluster_autoscaler_assume_role_policy.json
+  name               = "${var.cluster_name}-cluster_autoscaler-role"
+}
+
+resource "aws_iam_policy" "cluster_autoscaler" {
+  name = "${var.cluster_name}-cluster_autoscaler-policy"
+
+  policy = jsonencode({
+    Statement = [{
+      Action = [
+        "autoscaling:DescribeAutoScalingGroups",
+        "autoscaling:DescribeAutoScalingInstances",
+        "autoscaling:DescribeLaunchConfigurations",
+        "autoscaling:DescribeTags",
+        "autoscaling:SetDesiredCapacity",
+        "autoscaling:TerminateInstanceInAutoScalingGroup",
+        "ec2:DescribeLaunchTemplateVersions",
+        "ec2:DescribeInstanceTypes"
+      ]
+      Effect   = "Allow"
+      Resource = "*"
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_autoscaler" {
+  role       = aws_iam_role.cluster_autoscaler.name
+  policy_arn = aws_iam_policy.cluster_autoscaler.arn
+}
+
+output "cluster_autoscaler_arn" {
+  value = aws_iam_role.cluster_autoscaler.arn
+}
